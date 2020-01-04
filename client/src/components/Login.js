89e65c895ef1bf1bc3551b540c3bba72b9f68700
Ryan Floyd <mrryanfloyd@github.com>
2020-01-04 17:21:31 -0500

    Fixed login logout links

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -170,7 +170,7 @@ class Login extends Component {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="register" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -184,7 +184,7 @@ class Register extends Component {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="login" variant="body2">
                     {"Already have an account? Log In"}
                   </Link>
                 </Grid>
