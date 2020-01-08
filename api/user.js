87bb33ab75aa2b6caa5f90e91506b1e39b71f5d9
Ryan Floyd <arfloyd7@gmail.com>
2020-01-08 14:22:01 -0500

    Debugging user balance

--- api/user.js
+++ api/user.js
@@ -49,7 +49,8 @@ router.post("/", (req, res) => {
                 user: {
                   id: user.id,
                   name: user.name,
-                  email: user.email
+                  email: user.email,
+                  balance: user.balance
                 }
               });
             }
