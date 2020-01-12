bdcc2de674b8516a4b096cb5bafe409ac565e059
Ryan Floyd <arfloyd7@gmail.com>
2020-01-11 22:50:34 -0500

    Stock graph component

---
+++ client/src/components/StockGraph.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class StockGraph extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Current Portfolio value:<span>$100,000</span>
+        </h1>
+      </div>
+    );
+  }
+}
